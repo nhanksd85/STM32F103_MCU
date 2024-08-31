c5b6a0a6f81cf83ff7ca9b93d0a74c4f4fa186c6
MSI\KAI <nhanksd85@gmail.com>
2024-08-31 21:57:20 +0700

    Update git ignore

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -22,7 +22,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "exercise1.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
